969d086b38b9bad7bc470ab31d46fff145b13df1
Jacob Williams <jacob.t.williams@outlook.com>
2020-04-11 22:55:29 -0600

    Silkworm replaces block on use.

--- src/main/java/com/novamachina/ens/common/item/SilkWormItem.java
+++ src/main/java/com/novamachina/ens/common/item/SilkWormItem.java
@@ -2,10 +2,13 @@ package com.novamachina.ens.common.item;
 
 import com.novamachina.ens.common.setup.ModSetup;
 import com.novamachina.ens.common.utility.LogUtil;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.LeavesBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.Items;
 import net.minecraft.util.ActionResultType;
 
 public class SilkWormItem extends Item {
@@ -21,6 +24,9 @@ public class SilkWormItem extends Item {
             if (state.getBlock() instanceof LeavesBlock) {
                 LogUtil.info("INFEST LEAVES");
                 context.getItem().shrink(1);
+                // TODO: Replace dirt with infested leaves
+                Block.replaceBlock(context.getWorld().getBlockState(context.getPos()),
+                    Blocks.DIRT.getDefaultState(), context.getWorld(), context.getPos(), 1);
                 return ActionResultType.SUCCESS;
             }
             LogUtil.info("NOT LEAVES");
