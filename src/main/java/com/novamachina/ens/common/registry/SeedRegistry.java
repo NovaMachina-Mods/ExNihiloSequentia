f46dcaf1988e0c7e33d248e998743a24fcbcd17b
Jacob Williams <jacob.t.williams@outlook.com>
2020-04-21 19:17:40 -0600

    Fix issue where tree seeds would crash.

--- src/main/java/com/novamachina/ens/common/registry/SeedRegistry.java
+++ src/main/java/com/novamachina/ens/common/registry/SeedRegistry.java
@@ -1,6 +1,5 @@
 package com.novamachina.ens.common.registry;
 
-import com.novamachina.ens.ExNihiloSequentia;
 import com.novamachina.ens.common.registry.registryitem.SeedRegistryItem;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.PlantType;
@@ -14,12 +13,19 @@ public class SeedRegistry extends IRegistry<SeedRegistryItem> {
 
     @Override
     protected void useDefaultRegistry() {
-        register("oak", new SeedRegistryItem(Blocks.OAK_SAPLING.getDefaultState(), null));
-        register("spruce", new SeedRegistryItem(Blocks.SPRUCE_SAPLING.getDefaultState(), null));
-        register("birch", new SeedRegistryItem(Blocks.BIRCH_SAPLING.getDefaultState(), null));
-        register("jungle", new SeedRegistryItem(Blocks.JUNGLE_SAPLING.getDefaultState(), null));
-        register("acacia", new SeedRegistryItem(Blocks.ACACIA_SAPLING.getDefaultState(), null));
-        register("darkoak", new SeedRegistryItem(Blocks.DARK_OAK_SAPLING.getDefaultState(), null));
+        // TODO: Add berry seeds
+        register("oak",
+            new SeedRegistryItem(Blocks.OAK_SAPLING.getDefaultState(), PlantType.Plains));
+        register("spruce",
+            new SeedRegistryItem(Blocks.SPRUCE_SAPLING.getDefaultState(), PlantType.Plains));
+        register("birch",
+            new SeedRegistryItem(Blocks.BIRCH_SAPLING.getDefaultState(), PlantType.Plains));
+        register("jungle",
+            new SeedRegistryItem(Blocks.JUNGLE_SAPLING.getDefaultState(), PlantType.Plains));
+        register("acacia",
+            new SeedRegistryItem(Blocks.ACACIA_SAPLING.getDefaultState(), PlantType.Plains));
+        register("darkoak",
+            new SeedRegistryItem(Blocks.DARK_OAK_SAPLING.getDefaultState(), PlantType.Plains));
         register("cactus", new SeedRegistryItem(Blocks.CACTUS.getDefaultState(), PlantType.Desert));
         register("sugarcane",
             new SeedRegistryItem(Blocks.SUGAR_CANE.getDefaultState(), PlantType.Beach));
