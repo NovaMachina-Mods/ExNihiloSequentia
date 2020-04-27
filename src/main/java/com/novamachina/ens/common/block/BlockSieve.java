87a334a3ce3635ba5b295c8db010b06b9625228d
Jacob Williams <jacob.t.williams@outlook.com>
2020-04-27 16:31:10 -0600

    Update Sieve to use mesh property.

--- src/main/java/com/novamachina/ens/common/block/BlockSieve.java
+++ src/main/java/com/novamachina/ens/common/block/BlockSieve.java
@@ -1,18 +1,28 @@
 package com.novamachina.ens.common.block;
 
 import com.novamachina.ens.common.builder.BlockBuilder;
+import com.novamachina.ens.common.item.mesh.EnumMesh;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.EnumProperty;
+import net.minecraft.state.IProperty;
+import net.minecraft.state.StateContainer.Builder;
 import net.minecraftforge.common.ToolType;
 
 public class BlockSieve extends BaseBlock {
+
+    public static final EnumProperty<EnumMesh> MESH = EnumProperty.create("mesh", EnumMesh.class);
 
     public BlockSieve() {
         super(new BlockBuilder().properties(
             Block.Properties.create(Material.WOOD).hardnessAndResistance(0.7F)
                 .sound(SoundType.WOOD).notSolid()).harvestLevel(ToolType.AXE, 0));
+        this.setDefaultState(this.stateContainer.getBaseState().with(MESH, EnumMesh.NONE));
     }
 
-
+    protected void fillStateContainer(Builder<Block, BlockState> builder) {
+        builder.add(MESH);
+    }
 }
---
+++ src/main/java/com/novamachina/ens/common/item/mesh/EnumMesh.java
@@ -0,0 +1,23 @@
+package com.novamachina.ens.common.item.mesh;
+
+import net.minecraft.util.IStringSerializable;
+
+public enum EnumMesh implements IStringSerializable {
+    NONE(0, "none"), STRING(1, "string"), FLINT(2, "flint"), IRON(3, "iron"), DIAMOND(4, "diamond");
+
+    private final int    id;
+    private final String name;
+
+    EnumMesh(int id, String name) {
+        this.id   = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
