baaadda76cf502af2ff2631040799bec5b77bd2d
Jacob Williams <jacob.t.williams@outlook.com>
2020-08-03 10:26:01 -0600

    Add welcome message

--- src/main/java/com/novamachina/exnihilosequentia/ExNihiloSequentia.java
+++ src/main/java/com/novamachina/exnihilosequentia/ExNihiloSequentia.java
@@ -7,8 +7,13 @@ import com.novamachina.exnihilosequentia.common.utility.Config;
 import com.novamachina.exnihilosequentia.common.utility.Constants;
 import com.novamachina.exnihilosequentia.common.utility.Constants.ModInfo;
 import com.novamachina.exnihilosequentia.common.utility.LogUtil;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -41,6 +46,18 @@ public class ExNihiloSequentia {
             event.getRegistry()
                 .register(new UseHammerModifier.Serializer()
                     .setRegistryName(ModInfo.MOD_ID, "use_hammer"));
+        }
+    }
+
+    @EventBusSubscriber(modid = ModInfo.MOD_ID, bus = Bus.FORGE)
+    public static class PlayerMessages {
+        @SubscribeEvent
+        public static void sendDevelopmentMessage(PlayerEvent.PlayerLoggedInEvent event) {
+            event.getPlayer().sendMessage(new TranslationTextComponent("message.new_mod"));
+            event.getPlayer().sendMessage(new TranslationTextComponent("message.issues"));
+            event.getPlayer().sendMessage(new TranslationTextComponent("message.issues_link")
+                .setStyle(new Style().setUnderlined(true)
+                    .setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/NovaMachina/ExNihiloSequentia/issues"))));
         }
     }
 }
