9a4239bfa8ee96919a6f29559ab800cb5becc744
Jacob Williams <jacob.t.williams@outlook.com>
2020-08-04 17:29:08 -0600

    Fluid Transform Recipes read from file.

--- src/main/java/com/novamachina/exnihilosequentia/common/tileentity/barrel/transform/FluidTransformRegistry.java
+++ src/main/java/com/novamachina/exnihilosequentia/common/tileentity/barrel/transform/FluidTransformRegistry.java
@@ -1,7 +1,13 @@
 package com.novamachina.exnihilosequentia.common.tileentity.barrel.transform;
 
+import com.google.gson.Gson;
+import com.novamachina.exnihilosequentia.common.json.BarrelRegistriesJson;
+import com.novamachina.exnihilosequentia.common.json.FluidBlockJson;
 import com.novamachina.exnihilosequentia.common.json.FluidTransformJson;
 import com.novamachina.exnihilosequentia.common.setup.ModFluids;
+import com.novamachina.exnihilosequentia.common.utility.Config;
+import com.novamachina.exnihilosequentia.common.utility.Constants;
+import com.novamachina.exnihilosequentia.common.utility.LogUtil;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.fluid.Fluid;
@@ -9,6 +15,9 @@ import net.minecraft.fluid.Fluids;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -45,6 +54,54 @@ public class FluidTransformRegistry {
     }
 
     public static void initialize() {
+        if(Config.USE_JSON_REGISTRIES.get()) {
+            addEntries(readJson());
+        } else {
+            useDefaults();
+        }
+    }
+
+    private static void addEntries(BarrelRegistriesJson registriesJson) {
+        for(FluidTransformJson entry : registriesJson.getFluidTransformRegistry()) {
+            if(itemExists(entry.getFluidInBarrel())) {
+                ResourceLocation fluidID = new ResourceLocation(entry.getFluidInBarrel());
+                if(itemExists(entry.getBlockBelow())) {
+                    ResourceLocation inputID = new ResourceLocation(entry.getBlockBelow());
+                    if(itemExists(entry.getResult())) {
+                        ResourceLocation resultID = new ResourceLocation(entry.getResult());
+                        addRecipe(fluidID, inputID, resultID);
+                    }else {
+                        LogUtil.warn(String.format("Entry \"%s\" does not exist...Skipping...", entry.getResult()));
+                    }
+                }else {
+                    LogUtil.warn(String.format("Entry \"%s\" does not exist...Skipping...", entry.getBlockBelow()));
+                }
+            } else {
+                LogUtil.warn(String.format("Entry \"%s\" does not exist...Skipping...", entry.getFluidInBarrel()));
+            }
+        }
+    }
+
+    private static boolean itemExists(String entry) {
+        ResourceLocation itemID = new ResourceLocation(entry);
+        return ForgeRegistries.BLOCKS.containsKey(itemID) || ForgeRegistries.ITEMS.containsKey(itemID) || ForgeRegistries.FLUIDS.containsKey(itemID);
+    }
+
+    private static BarrelRegistriesJson readJson() {
+        Path path = Constants.Json.baseJsonPath.resolve(Constants.Json.BARREL_FILE);
+        BarrelRegistriesJson barrelRegistriesJson = null;
+        try {
+            StringBuilder builder = new StringBuilder();
+            Files.readAllLines(path).forEach(builder::append);
+            barrelRegistriesJson = new Gson().fromJson(builder.toString(), BarrelRegistriesJson.class);
+            LogUtil.info(builder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return barrelRegistriesJson;
+    }
+
+    public static void useDefaults() {
         addRecipe(Fluids.WATER, Blocks.MYCELIUM, ModFluids.WITCH_WATER_STILL.get());
     }
 
