52e13af947e10347a5a7c3316d031ee1b43fc89e
Jacob Williams <jacob.t.williams@outlook.com>
2020-09-12 13:40:18 -0600

    Add reload command for registries.

--- src/main/java/com/novamachina/exnihilosequentia/common/init/ModInitialization.java
+++ src/main/java/com/novamachina/exnihilosequentia/common/init/ModInitialization.java
@@ -44,7 +44,9 @@ public class ModInitialization {
     }
 
     @SubscribeEvent
-    public static void setupTagBasedRegistries(FMLServerStartingEvent event) {
+    public static void onServerStart(FMLServerStartingEvent event) {
+        ModCommands.register(event.getCommandDispatcher());
+
         if (Config.USE_JSON_REGISTRIES.get()) {
             ExNihiloRegistries.BUS.useJson();
         } else {
