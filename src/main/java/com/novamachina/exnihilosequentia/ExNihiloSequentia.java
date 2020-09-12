34f462a67eab98fed2012b66035800182a35f570
Jacob Williams <jacob.t.williams@outlook.com>
2020-09-12 14:52:11 -0600

    Disable world enter messages.

--- src/main/java/com/novamachina/exnihilosequentia/ExNihiloSequentia.java
+++ src/main/java/com/novamachina/exnihilosequentia/ExNihiloSequentia.java
@@ -47,16 +47,16 @@ public class ExNihiloSequentia {
         }
     }
 
-    @EventBusSubscriber(modid = Constants.ModIds.EX_NIHILO_SEQUENTIA, bus = Bus.FORGE)
-    public static class PlayerMessages {
-        @SubscribeEvent
-        public static void sendDevelopmentMessage(PlayerEvent.PlayerLoggedInEvent event) {
-            event.getPlayer().sendMessage(new TranslationTextComponent("message.new_mod"), UUID.randomUUID());
-            event.getPlayer().sendMessage(new TranslationTextComponent("message.issues"), UUID.randomUUID());
-            event.getPlayer().sendMessage(new TranslationTextComponent("message.issues_link")
-                .func_230530_a_(Style.field_240709_b_.setUnderlined(true)
-                    .func_240715_a_(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/NovaMachina/ExNihiloSequentia/issues"))), UUID
-                .randomUUID());
-        }
-    }
+//    @EventBusSubscriber(modid = Constants.ModIds.EX_NIHILO_SEQUENTIA, bus = Bus.FORGE)
+//    public static class PlayerMessages {
+//        @SubscribeEvent
+//        public static void sendDevelopmentMessage(PlayerEvent.PlayerLoggedInEvent event) {
+//            event.getPlayer().sendMessage(new TranslationTextComponent("message.new_mod"), UUID.randomUUID());
+//            event.getPlayer().sendMessage(new TranslationTextComponent("message.issues"), UUID.randomUUID());
+//            event.getPlayer().sendMessage(new TranslationTextComponent("message.issues_link")
+//                .func_230530_a_(Style.field_240709_b_.setUnderlined(true)
+//                    .func_240715_a_(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/NovaMachina/ExNihiloSequentia/issues"))), UUID
+//                .randomUUID());
+//        }
+//    }
 }
