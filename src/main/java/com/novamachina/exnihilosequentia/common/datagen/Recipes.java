55e32a6ddc42f38aaa7d649f9652f510a61dbc72
Jacob Williams <jacob.t.williams@outlook.com>
2020-10-07 22:50:25 -0600

    Add barrel recipes to Recipe generator

--- src/main/java/com/novamachina/exnihilosequentia/common/datagen/Recipes.java
+++ src/main/java/com/novamachina/exnihilosequentia/common/datagen/Recipes.java
@@ -1,15 +1,23 @@
 package com.novamachina.exnihilosequentia.common.datagen;
 
+import com.novamachina.exnihilosequentia.common.block.BaseBlock;
+import com.novamachina.exnihilosequentia.common.init.ModBlocks;
 import com.novamachina.exnihilosequentia.common.item.pebbles.EnumPebbleType;
 import com.novamachina.exnihilosequentia.common.item.tools.crook.EnumCrook;
 import com.novamachina.exnihilosequentia.common.utility.Constants;
 import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
 import net.minecraft.data.ShapedRecipeBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.ItemTags;
+import net.minecraftforge.common.Tags;
 
 import java.util.function.Consumer;
 
@@ -21,6 +29,56 @@ public class Recipes extends RecipeProvider {
     @Override
     protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
         registerCrooks(consumer);
+        registerPebbleBlocks(consumer);
+        registerBarrels(consumer);
+    }
+
+    private void registerBarrels(Consumer<IFinishedRecipe> consumer) {
+        registerBarrel(ModBlocks.BARREL_STONE.get(), Blocks.STONE, Blocks.STONE_SLAB, consumer);
+        registerBarrel(ModBlocks.BARREL_WOOD.get(), ItemTags.PLANKS, ItemTags.WOODEN_SLABS, consumer);
+    }
+
+    private void registerBarrel(Block result, ITag.INamedTag<Item> walls, ITag.INamedTag<Item> base, Consumer<IFinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shapedRecipe(result)
+            .patternLine("x x")
+            .patternLine("x x")
+            .patternLine("x-x")
+            .key('x', walls)
+            .key('-', base)
+            .setGroup(Constants.ModIds.EX_NIHILO_SEQUENTIA)
+            .addCriterion("has_walls", hasItem(walls))
+            .addCriterion("has_base", hasItem(base))
+            .build(consumer);
+    }
+
+    private void registerBarrel(Block result, Block walls, Block base, Consumer<IFinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shapedRecipe(result)
+            .patternLine("x x")
+            .patternLine("x x")
+            .patternLine("x-x")
+            .key('x', walls)
+            .key('-', base)
+            .setGroup(Constants.ModIds.EX_NIHILO_SEQUENTIA)
+            .addCriterion("has_walls", InventoryChangeTrigger.Instance.forItems(walls))
+            .addCriterion("has_base", InventoryChangeTrigger.Instance.forItems(base))
+            .build(consumer);
+    }
+
+    private void registerPebbleBlocks(Consumer<IFinishedRecipe> consumer) {
+        registerPebbleBlock(Blocks.ANDESITE, EnumPebbleType.ANDESITE.getRegistryObject().get(), consumer);
+        registerPebbleBlock(Blocks.COBBLESTONE, EnumPebbleType.STONE.getRegistryObject().get(), consumer);
+        registerPebbleBlock(Blocks.DIORITE, EnumPebbleType.DIORITE.getRegistryObject().get(), consumer);
+        registerPebbleBlock(Blocks.GRANITE, EnumPebbleType.GRANITE.getRegistryObject().get(), consumer);
+    }
+
+    private void registerPebbleBlock(Block result, Item input, Consumer<IFinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shapedRecipe(result)
+            .patternLine("xx")
+            .patternLine("xx")
+            .key('x', input)
+            .setGroup(Constants.ModIds.EX_NIHILO_SEQUENTIA)
+            .addCriterion("has_pebble", InventoryChangeTrigger.Instance.forItems(input))
+            .build(consumer);
     }
 
     private void registerCrooks(Consumer<IFinishedRecipe> consumer) {
