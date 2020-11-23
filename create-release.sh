4e082065e23b81d7872d7b52968d557d713dc53b
Jacob Williams <jacob.t.williams@outlook.com>
2020-11-23 15:38:05 -0700

    Update create-release.sh

--- create-release.sh
+++ create-release.sh
@@ -1,6 +1,7 @@
 getVersion() {
   version=$(grep mc_major gradle.properties)
-  mc_major=${version:: -5}
+  mc_major=$( echo -n $version | tail -c 5)
+  echo $mc_major
   version=${mc_major//[$'\t\r\n']}
   mod_version="-$(head -n 1 changelog.md)"
   full_version=$version$mod_version
