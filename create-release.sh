6697f4666607b9d1c0817061c36f4d81212f2ae7
Jacob Williams <jacob.t.williams@outlook.com>
2020-11-23 19:25:36 -0700

    Update release script

--- create-release.sh
+++ create-release.sh
@@ -1,10 +1,10 @@
 getVersion() {
-  version=$(grep mc_major gradle.properties)
-  mc_major=$( echo -n $version | tail -c 4)
+  version_line=$(grep mc_major gradle.properties)
+  mc_major=$( echo -n $version_line | tail -c 4)
   echo $mc_major
   mod_version="-$(head -n 1 changelog.md)"
   echo $mod_version
-  full_version=$version$mod_version
+  full_version=mc_major$mod_version
   echo $full_version
 }
 
@@ -14,5 +14,5 @@ getDescription() {
 
 getVersion
 getDescription
-github-release release --user NovaMachina --repo ExNihiloSequentia --tag $full_version --name $full_version --description $description
-github-release upload --user NovaMachina --repo ExNihiloSequentia --tag $full_version --name "$full_version.jar" --file build/libs/*.jar
+github-release release --user NovaMachina --repo ExNihiloSequentia --tag "$full_version" --name "$full_version" --description "$description"
+github-release upload --user NovaMachina --repo ExNihiloSequentia --tag "$full_version" --name "$full_version.jar" --file build/libs/*.jar
