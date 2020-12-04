c789c83b8db4f9f41b41f6e2278cf1b51791d9d9
Gerald Baggett <raziel23x@gmail.com>
2020-12-04 12:27:57 -0500

    moved to datagen as requested

--- src/main/java/novamachina/exnihilosequentia/common/datagen/Recipes.java
+++ src/main/java/novamachina/exnihilosequentia/common/datagen/Recipes.java
@@ -191,6 +191,13 @@ public class Recipes extends RecipeProvider {
             .addResult(Items.BLAZE_POWDER)
             .addRoll(new MeshWithChance(EnumMesh.IRON, 0.05F))
             .build(consumer, sieveLoc("blaze_powder"));
+        SieveRecipeBuilder.builder().input(Ingredient.fromItems(Blocks.SOUL_SAND))
+                .addResult(Items.ENDER_PEARL)
+                .addRoll(new MeshWithChance(EnumMesh.IRON, 0.005F))
+                .addRoll(new MeshWithChance(EnumMesh.DIAMOND, 0.01F))
+                .addRoll(new MeshWithChance(EnumMesh.EMERALD, 0.015F))
+                .addRoll(new MeshWithChance(EnumMesh.NETHERITE, 0.02F))
+                .build(consumer, sieveLoc("ender_pearl"));
 
         for(EnumOre ore : EnumOre.values()) {
             switch (ore) {
