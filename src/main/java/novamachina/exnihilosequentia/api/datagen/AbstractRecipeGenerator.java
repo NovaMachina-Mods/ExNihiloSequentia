17d0d385101a60fdb9cd1a3437a795b7643d0ef4
Jacob Williams <jacob.t.williams@outlook.com>
2020-12-24 14:59:26 -0600

    Move ore recipe generation to super class

--- src/main/java/novamachina/exnihilosequentia/api/datagen/AbstractRecipeGenerator.java
+++ src/main/java/novamachina/exnihilosequentia/api/datagen/AbstractRecipeGenerator.java
@@ -1,11 +1,22 @@
 package novamachina.exnihilosequentia.api.datagen;
 
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.data.CookingRecipeBuilder;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
+import novamachina.exnihilosequentia.common.item.ore.EnumOre;
+import novamachina.exnihilosequentia.common.utility.ExNihiloConstants;
+
+import java.util.function.Consumer;
 
 public abstract class AbstractRecipeGenerator extends RecipeProvider {
     private final String modId;
+
+    protected static final String CHUNK_CONDITION = "has_chunk";
 
     protected AbstractRecipeGenerator(DataGenerator generator, String modId) {
         super(generator);
@@ -46,6 +57,23 @@ public abstract class AbstractRecipeGenerator extends RecipeProvider {
 
     protected ResourceLocation hammerLoc(String id) {
         return new ResourceLocation(modId, "hammer/" + id);
+    }
+
+    protected void registerSmelting(EnumOre ore, Consumer<IFinishedRecipe> consumer) {
+        CookingRecipeBuilder
+            .smeltingRecipe(Ingredient.fromItems(ore.getChunkItem().get()), ore.getIngotItem(), 0.7F, 200)
+            .addCriterion(CHUNK_CONDITION, InventoryChangeTrigger.Instance.forItems(ore.getChunkItem().get()))
+            .build(consumer);
+    }
+
+    protected void registerOre(EnumOre ore, Consumer<IFinishedRecipe> consumer) {
+        ShapedRecipeBuilder.shapedRecipe(ore.getChunkItem().get())
+            .patternLine("xx")
+            .patternLine("xx")
+            .key('x', ore.getPieceItem().get())
+            .setGroup(ExNihiloConstants.ModIds.EX_NIHILO_SEQUENTIA)
+            .addCriterion("has_piece", InventoryChangeTrigger.Instance.forItems(ore.getPieceItem().get()))
+            .build(consumer);
     }
 
     @Override
