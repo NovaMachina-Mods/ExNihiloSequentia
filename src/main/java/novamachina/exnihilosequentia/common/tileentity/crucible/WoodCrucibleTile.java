d08dda07b8d40134f49eb6e85d0efc87cc587c70
Jacob Williams <jacob.t.williams@outlook.com>
2021-02-28 12:48:53 -0700

    Fix #127

--- src/main/java/novamachina/exnihilosequentia/common/tileentity/crucible/WoodCrucibleTile.java
+++ src/main/java/novamachina/exnihilosequentia/common/tileentity/crucible/WoodCrucibleTile.java
@@ -75,8 +75,8 @@ public class WoodCrucibleTile extends BaseCrucibleTile {
 
     @Override
     public int getHeat() {
-        return ExNihiloRegistries.HEAT_REGISTRY
-            .getHeatAmount(world.getBlockState(pos.down()).getBlock()) > 0 ? Config.getWoodHeatRate() : 0;
+        int blockHeat = ExNihiloRegistries.HEAT_REGISTRY.getHeatAmount(world.getBlockState(pos.down()).getBlock());
+        return Math.max(blockHeat, Config.getWoodHeatRate());
     }
 
     @Override
