3b126479f6c92023c806edd2bc97b14258351685
remplerus <44590574+remplerus@users.noreply.github.com>
2021-04-16 12:29:35 +0200

    Update PebbleItem.java

--- src/main/java/novamachina/exnihilosequentia/common/item/pebbles/PebbleItem.java
+++ src/main/java/novamachina/exnihilosequentia/common/item/pebbles/PebbleItem.java
@@ -1,19 +1,8 @@
 package novamachina.exnihilosequentia.common.item.pebbles;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.SnowballEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUseContext;
 import net.minecraft.item.SnowballItem;
-import net.minecraft.stats.Stats;
-import net.minecraft.util.*;
-import net.minecraft.util.math.EntityRayTraceResult;
-import net.minecraft.world.World;
 import novamachina.exnihilosequentia.common.init.ExNihiloInitialization;
-import novamachina.exnihilosequentia.common.init.ExNihiloSounds;
-import novamachina.exnihilosequentia.common.utility.Config;
 
 public class PebbleItem extends SnowballItem {
     public PebbleItem() {
