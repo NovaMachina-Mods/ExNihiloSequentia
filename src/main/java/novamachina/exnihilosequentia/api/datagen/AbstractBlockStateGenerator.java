aa00e724e61719adb3cb00d6f632cd75a2526f5c
remplerus <44590574+remplerus@users.noreply.github.com>
2021-05-07 21:55:54 +0200

    make addons easier to create

--- src/main/java/novamachina/exnihilosequentia/api/datagen/AbstractBlockStateGenerator.java
+++ src/main/java/novamachina/exnihilosequentia/api/datagen/AbstractBlockStateGenerator.java
@@ -4,16 +4,23 @@ import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.generators.BlockStateProvider;
-import net.minecraftforge.client.model.generators.ConfiguredModel;
-import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.client.model.generators.*;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import novamachina.exnihilosequentia.common.block.BlockSieve;
+import novamachina.exnihilosequentia.common.item.mesh.EnumMesh;
+import novamachina.exnihilosequentia.common.utility.ExNihiloConstants;
+
+import static net.minecraft.block.CakeBlock.BITES;
 
 public abstract class AbstractBlockStateGenerator extends BlockStateProvider {
     protected static final String PARTICLE_TAG = "particle";
 
     protected AbstractBlockStateGenerator(DataGenerator gen, String modId, ExistingFileHelper exFileHelper) {
         super(gen, modId, exFileHelper);
+    }
+
+    protected ResourceLocation exnihiloLoc(String string) {
+        return new ResourceLocation(ExNihiloConstants.ModIds.EX_NIHILO_SEQUENTIA, string);
     }
 
     protected void basicBlock(Block block) {
@@ -31,5 +38,60 @@ public abstract class AbstractBlockStateGenerator extends BlockStateProvider {
     protected void simpleItemBlock(Block block, ModelFile modelFile) {
         simpleBlock(block, modelFile);
         simpleBlockItem(block, modelFile);
+    }
+
+    protected String getRegistryName(Block b) {
+        return b.getRegistryName().toString();
+    }
+
+    protected void createBarrel(Block block, ResourceLocation texture) {
+        ConfiguredModel model = new ConfiguredModel(models().withExistingParent(getRegistryName(block), exnihiloLoc("block/barrel"))
+                .texture("texture", texture)
+                .texture(PARTICLE_TAG, texture));
+
+        simpleItemBlock(block, model.model);
+    }
+
+    protected void createCake(Block block) {
+        VariantBlockStateBuilder builder = getVariantBuilder(block);
+
+        for (int i = 1; i < 7; i++) {
+            VariantBlockStateBuilder.PartialBlockstate partialBlockstate = builder.partialState();
+            ConfiguredModel model = new ConfiguredModel(models().getExistingFile(modLoc("block/cake_slice" + i)));
+            partialBlockstate.with(BITES, i).addModels(model);
+        }
+        VariantBlockStateBuilder.PartialBlockstate partialBlockstate = builder.partialState();
+        ConfiguredModel model = new ConfiguredModel(models().getExistingFile(modLoc("block/cake_uneaten")));
+        partialBlockstate.with(BITES, 0).addModels(model);
+    }
+
+    protected void createCrucible(Block block, ResourceLocation texture) {
+        ConfiguredModel model = new ConfiguredModel(models().withExistingParent(getRegistryName(block), modLoc("block/crucible"))
+                .texture(PARTICLE_TAG, texture)
+                .texture("top", texture)
+                .texture("bottom", texture)
+                .texture("side", texture)
+                .texture("inside", texture));
+
+        simpleItemBlock(block, model.model);
+    }
+
+    protected void createSieve(Block block, ResourceLocation texture) {
+        ConfiguredModel model = new ConfiguredModel(models().withExistingParent(getRegistryName(block), modLoc("block/sieve_base"))
+                .texture("texture", texture)
+                .texture(PARTICLE_TAG, texture));
+        MultiPartBlockStateBuilder builder = getMultipartBuilder(block);
+        MultiPartBlockStateBuilder.PartBuilder partBuilder = builder.part().modelFile(model.model).addModel();
+        partBuilder.end();
+
+        for (EnumMesh mesh : EnumMesh.values()) {
+            if (mesh != EnumMesh.NONE) {
+                partBuilder = builder.part()
+                        .modelFile(new ModelFile.ExistingModelFile(new ResourceLocation(ExNihiloConstants.ModIds.EX_NIHILO_SEQUENTIA, "block/" + mesh.getMeshName()), models().existingFileHelper)).addModel();
+                partBuilder.condition(BlockSieve.MESH, mesh);
+                partBuilder.end();
+            }
+        }
+        simpleBlockItem(block, model.model);
     }
 }
