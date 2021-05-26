b38bd42cc1abe334bc1d5c2b10e4ca42b6d9f675
lazynessmind <lazynessmind@gmail.com>
2021-05-27 00:23:43 +0100

    Change: new properties (isLiquid and a map that contains the heat for the fluid value)

--- src/main/java/novamachina/exnihilosequentia/api/crafting/heat/HeatRecipe.java
+++ src/main/java/novamachina/exnihilosequentia/api/crafting/heat/HeatRecipe.java
@@ -9,16 +9,29 @@ import novamachina.exnihilosequentia.api.crafting.RecipeSerializer;
 import novamachina.exnihilosequentia.api.crafting.SerializableRecipe;
 import novamachina.exnihilosequentia.common.utility.ExNihiloConstants;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HeatRecipe extends SerializableRecipe {
     public static final IRecipeType<HeatRecipe> RECIPE_TYPE = IRecipeType.register(ExNihiloConstants.ModIds.EX_NIHILO_SEQUENTIA + ":heat");
     private static RegistryObject<RecipeSerializer<HeatRecipe>> serializer;
     private int amount;
     private Block input;
 
+    private boolean isLiquid = false;
+    private Map<String, Integer> heatByLevel = new HashMap<>();
+
     public HeatRecipe(ResourceLocation id, Block input, int amount) {
         super(null, RECIPE_TYPE, id);
         this.input = input;
         this.amount = amount;
+        this.heatByLevel.put("*", amount);
+    }
+
+    public HeatRecipe(ResourceLocation id, Block input, int amount, boolean isLiquid, Map<String, Integer> heatByLevel){
+        this(id, input, amount);
+        this.isLiquid = isLiquid;
+        this.heatByLevel = heatByLevel;
     }
 
     public static RegistryObject<RecipeSerializer<HeatRecipe>> getStaticSerializer() {
@@ -43,6 +56,22 @@ public class HeatRecipe extends SerializableRecipe {
 
     public void setInput(Block input) {
         this.input = input;
+    }
+
+    public boolean isLiquid() {
+        return this.isLiquid;
+    }
+
+    public void setIsLiquid(boolean isLiquid) {
+        this.isLiquid = isLiquid;
+    }
+
+    public Map<String, Integer> getHeatByLevel() {
+        return this.heatByLevel;
+    }
+
+    public void setHeatByLevel(Map<String, Integer> heatByLevel) {
+        this.heatByLevel = heatByLevel;
     }
 
     @Override
