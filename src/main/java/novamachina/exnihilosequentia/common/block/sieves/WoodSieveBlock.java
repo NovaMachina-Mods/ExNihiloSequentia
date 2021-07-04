43f08c1f804d8dd28aa91cf67de54d6dbfbb7ac3
Jacob Williams <jacob.t.williams@outlook.com>
2021-07-04 08:55:29 -0500

    Change sieve sounds to use the scaffolding sounds.
    [Ticket: AB#41]

--- src/main/java/novamachina/exnihilosequentia/common/block/sieves/WoodSieveBlock.java
+++ src/main/java/novamachina/exnihilosequentia/common/block/sieves/WoodSieveBlock.java
@@ -12,7 +12,7 @@ public class WoodSieveBlock extends BlockSieve {
     public WoodSieveBlock() {
         super(new BlockBuilder().properties(
                 AbstractBlock.Properties.of(Material.WOOD).strength(0.7F)
-                        .sound(SoundType.WOOD).noOcclusion().isRedstoneConductor((state, reader, pos) -> false)
+                        .sound(SoundType.SCAFFOLDING).noOcclusion().isRedstoneConductor((state, reader, pos) -> false)
                         .isSuffocating((state, reader, pos) -> false).isViewBlocking((state, reader, pos) -> false))
                 .harvestLevel(ToolType.AXE, 0).tileEntitySupplier(SieveTile::new));
     }
