0f5ed365057fe823de97597b85894dc872f89305
remplerus <44590574+remplerus@users.noreply.github.com>
2021-11-25 21:33:10 +0100

    - make infested leaves only been broken fast by crooks - make infested leaves been dropped by shears (again)

--- src/main/java/novamachina/exnihilosequentia/common/block/InfestedLeavesBlock.java
+++ src/main/java/novamachina/exnihilosequentia/common/block/InfestedLeavesBlock.java
@@ -1,18 +1,38 @@
 package novamachina.exnihilosequentia.common.block;
 
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShearsItem;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.common.IForgeShearable;
+import net.minecraftforge.common.ToolType;
 import novamachina.exnihilosequentia.common.builder.BlockBuilder;
 import novamachina.exnihilosequentia.common.tileentity.InfestedLeavesTile;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class InfestedLeavesBlock extends BaseBlock implements IForgeShearable {
 
     public InfestedLeavesBlock() {
-        super(new BlockBuilder().properties(
+        super(new BlockBuilder().harvestLevel(ToolType.get("crook"), 0).properties(
                 AbstractBlock.Properties.of(Material.LEAVES).strength(0.2F).sound(
-                        SoundType.GRASS).noOcclusion().isValidSpawn(BaseBlock::never)).tileEntitySupplier(InfestedLeavesTile::new));
+                        SoundType.GRASS).noOcclusion()
+                        .isValidSpawn(BaseBlock::never)).tileEntitySupplier(InfestedLeavesTile::new));
+    }
+
+    @Override
+    public void playerDestroy(@Nonnull World world, PlayerEntity player, @Nonnull BlockPos pos, BlockState state, @Nullable TileEntity p_180657_5_, ItemStack itemStack) {
+        if (itemStack.getItem() instanceof ShearsItem) {
+            world.addFreshEntity(new ItemEntity(world, pos.getX() + 0.5F, pos.getY() + 1.1F,
+                    pos.getZ() + 0.5F, new ItemStack(this)));
+        }
     }
 }
