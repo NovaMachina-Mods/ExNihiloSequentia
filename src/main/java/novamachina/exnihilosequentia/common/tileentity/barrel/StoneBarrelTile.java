c8e13c97b984c29f4389a0105dbf9dda899fb0ad
remplerus <44590574+remplerus@users.noreply.github.com>
2021-12-07 09:19:16 +0100

    - optimize tiles for addons

--- src/main/java/novamachina/exnihilosequentia/common/tileentity/barrel/StoneBarrelTile.java
+++ src/main/java/novamachina/exnihilosequentia/common/tileentity/barrel/StoneBarrelTile.java
@@ -1,11 +1,16 @@
 package novamachina.exnihilosequentia.common.tileentity.barrel;
 
+import net.minecraft.tileentity.TileEntityType;
 import novamachina.exnihilosequentia.common.init.ExNihiloTiles;
 import net.minecraftforge.fluids.FluidStack;
 
 public class StoneBarrelTile extends AbstractBarrelTile{
+    public StoneBarrelTile(TileEntityType<? extends AbstractBarrelTile> tile) {
+        super(tile);
+    }
+
     public StoneBarrelTile() {
-        super(ExNihiloTiles.BARREL_STONE.get());
+        this(ExNihiloTiles.BARREL_STONE.get());
     }
 
     @Override
--- src/main/java/novamachina/exnihilosequentia/common/tileentity/barrel/WoodBarrelTile.java
+++ src/main/java/novamachina/exnihilosequentia/common/tileentity/barrel/WoodBarrelTile.java
@@ -1,12 +1,17 @@
 package novamachina.exnihilosequentia.common.tileentity.barrel;
 
+import net.minecraft.tileentity.TileEntityType;
 import novamachina.exnihilosequentia.common.init.ExNihiloTiles;
 import novamachina.exnihilosequentia.common.utility.Config;
 import net.minecraftforge.fluids.FluidStack;
 
 public class WoodBarrelTile extends AbstractBarrelTile {
     public WoodBarrelTile() {
-        super(ExNihiloTiles.BARREL_WOOD.get());
+        this(ExNihiloTiles.BARREL_WOOD.get());
+    }
+
+    public WoodBarrelTile(TileEntityType<? extends AbstractBarrelTile> tile) {
+        super(tile);
     }
 
     @Override
