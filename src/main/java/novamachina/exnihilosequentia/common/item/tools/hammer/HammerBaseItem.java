5c8d64523a064b23e42847d12242801e85e386fd
remplerus <44590574+remplerus@users.noreply.github.com>
2022-01-30 21:45:09 +0100

    clean up

--- src/main/java/novamachina/exnihilosequentia/common/item/tools/hammer/HammerBaseItem.java
+++ src/main/java/novamachina/exnihilosequentia/common/item/tools/hammer/HammerBaseItem.java
@@ -23,18 +23,6 @@ import javax.annotation.Nullable;
 
 public class HammerBaseItem extends ToolItem {
 
-    //TODO
-    /*@Nonnull private static final Set<Block> effectiveBlocksOn = Sets
-            .newHashSet(Blocks.STONE, Blocks.DIORITE, Blocks.ANDESITE, Blocks.GRANITE,
-                    Blocks.NETHERRACK, Blocks.END_STONE, Blocks.COBBLESTONE,
-                    ExNihiloBlocks.CRUSHED_DIORITE.get(), ExNihiloBlocks.CRUSHED_ANDESITE.get(),
-                    ExNihiloBlocks.CRUSHED_GRANITE.get(), ExNihiloBlocks.CRUSHED_NETHERRACK.get(),
-                    ExNihiloBlocks.CRUSHED_END_STONE.get(), Blocks.GRAVEL, Blocks.SAND,
-                    Blocks.TUBE_CORAL_BLOCK, Blocks.BRAIN_CORAL_BLOCK, Blocks.BUBBLE_CORAL_BLOCK,
-                    Blocks.FIRE_CORAL_BLOCK, Blocks.HORN_CORAL_BLOCK,
-                    Blocks.TUBE_CORAL, Blocks.BRAIN_CORAL, Blocks.BUBBLE_CORAL,
-                    Blocks.FIRE_CORAL, Blocks.HORN_CORAL);
-     */
     @Nonnull private static final Set<Block> effectiveBlocksOn = Sets.newHashSet();
 
     public HammerBaseItem(@Nonnull final IItemTier tier, final int maxDamage) {
@@ -65,7 +53,6 @@ public class HammerBaseItem extends ToolItem {
             if (blockString != null) {
                 String[] temp = blockString.split(":");
                 ResourceLocation location = new ResourceLocation(temp[0], temp[1]);
-                //TODO not 100% sure
                 if (ForgeRegistries.BLOCKS.getValue(location) != null) {
                     effectiveBlocksOn.add(ForgeRegistries.BLOCKS.getValue(location));
                 }
