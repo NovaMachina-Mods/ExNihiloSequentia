9085d2efc29657b5c2d8d2a07ba1b6ac491cf426
remplerus <44590574+remplerus@users.noreply.github.com>
2022-03-12 04:24:17 +0100

    - make custom crook recipes not drop block if broken

--- src/main/java/novamachina/exnihilosequentia/common/loot/modifier/UseCrookModifier.java
+++ src/main/java/novamachina/exnihilosequentia/common/loot/modifier/UseCrookModifier.java
@@ -82,9 +82,11 @@ public class UseCrookModifier extends LootModifier {
                 newLoot.add(new ItemStack(Items.STRING, random
                         .nextInt(Config.getMaxBonusStringCount()) + Config.getMinStringCount()));
                 if (random.nextDouble() <= 0.8) {
-                    newLoot
-                            .add(new ItemStack(ExNihiloItems.SILKWORM.get()));
+                    newLoot.add(new ItemStack(ExNihiloItems.SILKWORM.get()));
                 }
+            }
+            if (newLoot.isEmpty()) {
+                return new ArrayList<>();
             }
         }
         if (!newLoot.isEmpty()) {
