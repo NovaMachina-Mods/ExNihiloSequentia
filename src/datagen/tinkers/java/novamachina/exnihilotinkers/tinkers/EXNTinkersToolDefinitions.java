f5bf6abe820bef76b8b28c4f614693f0104d4a03
remplerus <44590574+remplerus@users.noreply.github.com>
2022-07-02 16:14:49 +0200

    - crook and hammer now have a modifier on durability

--- src/datagen/tinkers/java/novamachina/exnihilotinkers/tinkers/EXNTinkersToolDefinitions.java
+++ src/datagen/tinkers/java/novamachina/exnihilotinkers/tinkers/EXNTinkersToolDefinitions.java
@@ -1,6 +1,9 @@
 package novamachina.exnihilotinkers.tinkers;
 
 import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
+import novamachina.exnihilosequentia.api.tag.ExNihiloTags;
 import novamachina.exnihilotinkers.common.tinkers.ToolDefinitions;
 import novamachina.exnihilotinkers.common.utility.EXNTinkersConstants;
 import slimeknights.tconstruct.library.data.tinkering.AbstractToolDefinitionDataProvider;
@@ -31,7 +34,10 @@ public class EXNTinkersToolDefinitions extends AbstractToolDefinitionDataProvide
                 .part(toolHandle)
                 .stat(ToolStats.ATTACK_DAMAGE, -2F)
                 .stat(ToolStats.ATTACK_SPEED, 3F)
+                .multiplier(ToolStats.DURABILITY, 1.4F)
                 .smallToolStartingSlots()
+                .action(ToolActions.SHEARS_DIG)
+                .effective(ExNihiloTags.MINEABLE_WITH_CROOK)
                 .build();
         define(ToolDefinitions.exnHammer)
                 .part(hammerHead)
@@ -39,7 +45,10 @@ public class EXNTinkersToolDefinitions extends AbstractToolDefinitionDataProvide
                 .part(toolHandle)
                 .stat(ToolStats.ATTACK_DAMAGE, 0.5F)
                 .stat(ToolStats.ATTACK_SPEED, 1.2F)
+                .multiplier(ToolStats.DURABILITY, 2.2F)
                 .smallToolStartingSlots()
+                .action(ToolActions.PICKAXE_DIG)
+                .effective(ExNihiloTags.MINEABLE_WITH_HAMMER)
                 .build();
     }
 
