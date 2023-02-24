3d3bb4e77eb4c4fcb27e58f0c170ab6deaa4799d
jacob <jacob@TeamCity>
2023-02-24 00:35:25 +0000

    TeamCity change in 'NovaMachina Mods / ExNihiloSequentia' project: Templates associated with the build configuration 'Build Secondary Branches' have been reordered: Exclude Default Branch, Discord Notify, Build Main Branches, Publish' templates

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.projectFeatures.githubIssues
 
 /*
@@ -54,10 +55,22 @@ object Build : BuildType({
 })
 
 object BuildSecondaryBranches : BuildType({
-    templates(AbsoluteId("NovaMachinaMods_ExcludeDefaultBranch"), AbsoluteId("NovaMachinaMods_DiscordNotify"), AbsoluteId("NovaMachinaMods_BuildMainBranches"), AbsoluteId("NovaMachinaMods_Publish"))
+    templates(AbsoluteId("NovaMachinaMods_ExcludeDefaultBranch"), AbsoluteId("NovaMachinaMods_DiscordNotify"), AbsoluteId("NovaMachinaMods_BuildMainBranches"))
     name = "Build Secondary Branches"
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        gradle {
+            name = "Publish"
+            id = "RUNNER_3"
+            tasks = "publishCurseForge publishMainPublicationToMavenRepository"
+            buildFile = "build.gradle"
+            dockerImage = "gradle:7.6-jdk17"
+            dockerRunParameters = "-u root -v /home/buildagent/.gradle:/home/gradle/.gradle"
+        }
+        stepsOrder = arrayListOf("RUNNER_4", "RUNNER_7", "RUNNER_8", "RUNNER_3", "RUNNER_1", "RUNNER_5", "RUNNER_2")
     }
 })
